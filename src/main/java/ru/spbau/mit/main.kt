20ab27e625c4ed7787b9e12e9afa7afe472b358f
Stanislav Belyaev <stasbelyaev96@gmail.com>
2017-10-25 09:36:45 +0300

    Complete task01: problem solved and tests.

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,80 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+enum class PoS { ADJ, NOUN, VERB }
 
-    return words.joinToString(separator = " ")
+enum class Gender { MALE, FEMALE }
+
+data class Rule(val suffix: String, val pos: PoS, val gender: Gender)
+
+val rules = setOf(
+        Rule("lios", PoS.ADJ, Gender.MALE),
+        Rule("liala", PoS.ADJ, Gender.FEMALE),
+        Rule("etr", PoS.NOUN, Gender.MALE),
+        Rule("etra", PoS.NOUN, Gender.FEMALE),
+        Rule("initis", PoS.VERB, Gender.MALE),
+        Rule("inites", PoS.VERB, Gender.FEMALE)
+)
+
+class PWord(raw: String) {
+    private val firstRule by lazy {
+        rules.firstOrNull { raw.endsWith(it.suffix) }
+    }
+
+    val pos: PoS? by lazy {
+        firstRule?.pos
+    }
+
+    val gender: Gender? by lazy {
+        firstRule?.gender
+    }
 }
 
-fun main(args: Array<String>) {
-    println(getGreeting())
+class PMessage(words: List<PWord>) {
+    private val oneGoodWord by lazy {
+        (words.size == 1) && (words.single().let { (it.pos != null) && (it.gender != null) })
+    }
+
+    private val sameGender by lazy {
+        words.map { it.gender }.let { (it.all { it != null }) && (it.distinct().size == 1) }
+    }
+
+    private val notNounNumMatch by lazy {
+        val adjPrefixNum = words.map { it.pos }.takeWhile { it == PoS.ADJ }.size
+        val verbSuffixNum = words.map { it.pos }.reversed().takeWhile { it == PoS.VERB }.size
+        adjPrefixNum + verbSuffixNum == words.size - 1
+    }
+
+    private val singleNoun by lazy {
+        words.map { it.pos }.count { it == PoS.NOUN } == 1
+    }
+
+    val singleSentence: Boolean by lazy {
+        oneGoodWord || (sameGender && notNounNumMatch && singleNoun)
+    }
 }
+
+interface Solver {
+    fun solve(rawInput: String): String
+}
+
+// http://codeforces.com/problemset/problem/113/A
+object GrammarLessons : Solver {
+    private fun parseWords(rawInput: String): List<PWord> = rawInput.split(" ").map { PWord(it) }
+
+    override fun solve(rawInput: String): String {
+        val message = PMessage(words = parseWords(rawInput))
+        return if (message.singleSentence) "YES" else "NO"
+    }
+}
+
+interface Runner {
+    fun run(solver: Solver)
+}
+
+object ConsoleRunner : Runner {
+    private fun readRawInput(): String = readLine()!!
+
+    override fun run(solver: Solver) = print(solver.solve(readRawInput()))
+}
+
+fun main(args: Array<String>) = ConsoleRunner.run(GrammarLessons)
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,115 @@
 package ru.spbau.mit
-import kotlin.test.assertEquals
+
 import org.junit.Test
+import java.util.*
+import kotlin.test.assertEquals
 
 class TestSource {
+    private fun assertRaw(
+            expected: String,
+            rawInput: String,
+            message: String? = null,
+            solver: Solver = GrammarLessons
+    ) {
+        val runner = object : Runner {
+            override fun run(solver: Solver) = assertEquals(expected, solver.solve(rawInput), message)
+        }
+        runner.run(solver)
+    }
+
+    private object PCodedRandom {
+        private fun nextAlpha(): String {
+            return UUID.randomUUID().toString().filter { it.isLetter() && it.isLowerCase() }
+        }
+
+        private fun nextWord(pos: PoS, gender: Gender): String {
+            return nextAlpha() + rules.first { (it.pos == pos) && (it.gender == gender) }.suffix
+        }
+
+        private fun nextWords(desc: List<Pair<PoS, Gender>>): String {
+            return desc.joinToString(" ") { nextWord(it.first, it.second) }
+        }
+
+        private fun parseDesc(code: String): Pair<PoS, Gender> {
+            return when (code) {
+                "AM" -> Pair(PoS.ADJ, Gender.MALE)
+                "AF" -> Pair(PoS.ADJ, Gender.FEMALE)
+                "NM" -> Pair(PoS.NOUN, Gender.MALE)
+                "NF" -> Pair(PoS.NOUN, Gender.FEMALE)
+                "VM" -> Pair(PoS.VERB, Gender.MALE)
+                "VF" -> Pair(PoS.VERB, Gender.FEMALE)
+                else -> throw IllegalArgumentException("Wrong code")
+            }
+        }
+
+        fun next(codedInput: String): String {
+            return codedInput.split(" ").flatMap {
+                when {
+                    it.length > 2 -> {
+                        val num = Integer.valueOf(it.slice(0..(it.length - 3)))
+                        val desc = parseDesc(it.slice((it.length - 2)..(it.length - 1)))
+                        List(num) { desc }
+                    }
+                    else -> listOf(parseDesc(it))
+                }
+            }.let { nextWords(it) }
+        }
+    }
+
+    private fun assertCoded(
+            expected: String,
+            codedInput: String,
+            message: String? = null
+    ) = assertRaw(expected, PCodedRandom.next(codedInput), message)
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testSimple() {
+        assertRaw("NO", "a", "One word")
+        assertRaw("NO", "a b", "Two words")
+        assertRaw("NO", "a b c", "Three words")
+    }
+
+    @Test
+    fun testExamples() {
+        assertRaw("YES", "petr", "Example #1")
+        assertRaw("NO", "etis atis animatis etis atis amatis", "Example #2")
+        assertRaw("YES", "nataliala kataliala vetra feinites", "Example #3")
+    }
+
+    @Test
+    fun testSuffix() {
+        assertRaw("YES", "lios", "Labeled suffix #1")
+        assertRaw("YES", "liala", "Labeled suffix #2")
+        assertRaw("YES", "etr", "Labeled suffix #3")
+        assertRaw("YES", "etra", "Labeled suffix #4")
+        assertRaw("YES", "initis", "Labeled suffix #5")
+        assertRaw("YES", "inites", "Labeled suffix #6")
+    }
+
+    @Test
+    fun testComplex() {
+        assertRaw("NO", "asilios posinities", "Different genus")
+        assertRaw("NO", "perinitis giliala", "First verb, then adj")
+        assertRaw("YES", "fffetr", "Single noun")
+        assertRaw("YES", "alios blios clios ketr", "Zero verbs")
+        assertRaw("YES", "letra finites dinites", "Zero adjs")
+        assertRaw("NO", "ulios retr oetr sinitis jinitis", "More then one nouns")
+        assertRaw("NO", "jliala hinites kliala eetra", "Verb before noun")
+        assertRaw("NO", "eetra opainites wqeliala yinites", "Adj after noun")
+    }
+
+    @Test
+    fun testCoded() {
+        assertCoded("YES", "AM NM VM", "Simple male")
+        assertCoded("YES", "AF NF VF", "Simple female")
+        assertCoded("YES", "3AM NM 5VM", "Correct single sentence")
+        assertCoded("NO", "3AM NM 2VM VF 2VM", "One female genus and bunch of males")
+        assertCoded("NO", "111AM 345VM", "Absent of noun")
+        assertCoded("NO", "VM 567AM NM 987VM", "Verb before adj")
+        assertCoded("YES", "NF 1342VF", "Zero adjs")
+        assertCoded("YES", "11345AF NF", "Zero verbs")
+        assertCoded("NO", "11345AF NF 762AF NF 10VF", "Noun inside adjs")
+        assertCoded("NO", "980AM NM 555VM NM 345VM", "Noun inside verbs")
+        assertCoded("NO", "137AF 728AM 236AF NM 263VM 3216VF 7VM", "Mix of genders")
     }
 }
